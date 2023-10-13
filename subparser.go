cc1aa7e5f1af91a065ffcd0e19b949368fc7c1a4
Yannick <yan@effner.app>
2023-10-13 17:26:45 +0200

    added sources (file, dsb, effner.de) added parser for effner.de

--- subparser.go
+++ subparser.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"github.com/mkideal/cli"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
+	"strings"
 	"subparser/dsb"
 	"subparser/parsers"
 )
@@ -14,24 +20,27 @@ const (
 	ExitParserNotFound    = 2
 	ExitFileReadFailed    = 3
 	ExitDSBLoginFailed    = 4
-	ExitDSBLoadingFailed  = 5
+	ExitLoadingFailed     = 5
 	ExitParsingFailed     = 5
 	ExitFileWritingFailed = 6
 )
 
 type arguments struct {
 	cli.Helper
-	Parser  string `cli:"parser,P" usage:"name of the parser to use (default: effner)"`
-	Input   string `cli:"input,i" usage:"input file. If unset, DSB user and pass are required"`
-	DSBUser string `cli:"user,u" usage:"dsb username"`
-	DSBPass string `cli:"pass,p" usage:"dsb password"`
-	Output  string `cli:"output,o" usage:"file to output parsed data to (if unset SYSOUT is used)"`
+	Parser string `cli:"parser,P" usage:"name of the parser to use (default: effner) [effner, effner-de]"`
+	Input  string `cli:"input,i" usage:"input file (required if source is file)"`
+	Source string `cli:"source,s" usage:"source for the data (default: file) [file,dsb,effner]"`
+	User   string `cli:"user,u" usage:"username"`
+	Pass   string `cli:"pass,p" usage:"password"`
+	Output string `cli:"output,o" usage:"file to output parsed data to (if unset SYSOUT is used)"`
 }
 
 func getParser(parser string) parsers.Parser {
 	switch parser {
 	case "effner":
-		return &parsers.EffnerParser{}
+		return &parsers.EffnerDSBParser{}
+	case "effner-de":
+		return &parsers.EffnerDEParser{}
 	default:
 		return nil
 	}
@@ -43,25 +52,15 @@ func handle(error error, exit int) {
 }
 
 func main() {
+	// TODO REMOVE THIS REMOVE THIS THIS IS NOT WHAT WE DO USUALLY I SWEAR
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	os.Exit(cli.Run(new(arguments), func(ctx *cli.Context) error {
 		argv := ctx.Argv().(*arguments)
 
 		parserName := argv.Parser
 
-		inputFile := argv.Input
-		dsbUser := argv.DSBUser
-		dsbPass := argv.DSBPass
-
-		// validate input file and dsb credentials
-		if inputFile == "" && dsbUser == "" && dsbPass == "" {
-			fmt.Println("Error: Either input file or dsb credentials must be set!")
-			os.Exit(ExitInvalidArgs)
-		}
-
-		if inputFile != "" && (dsbUser != "" || dsbPass != "") {
-			fmt.Println("Error: Input file and DSB credentials cannot be defined at the same time!")
-			os.Exit(ExitInvalidArgs)
-		}
+		dsbUser := argv.User
+		dsbPass := argv.Pass
 
 		// validate and load parser
 		if parserName == "" {
@@ -79,7 +78,7 @@ func main() {
 		// prepare the data to parse
 		var data string
 
-		if argv.Input != "" {
+		if argv.Source == "file" {
 			// load data from file
 			content, err := os.ReadFile(argv.Input)
 
@@ -88,7 +87,12 @@ func main() {
 			}
 
 			data = string(content)
-		} else {
+		} else if argv.Source == "dsb" {
+			if argv.User == "" || argv.Pass == "" {
+				fmt.Println("Error: DSB-Source requires credentials!")
+				os.Exit(ExitInvalidArgs)
+			}
+
 			// load the data from DSB
 			dsbInstance := dsb.NewDSB(dsbUser, dsbPass)
 
@@ -101,7 +105,7 @@ func main() {
 			err = dsbInstance.LoadTimetables()
 
 			if err != nil {
-				handle(err, ExitDSBLoadingFailed)
+				handle(err, ExitLoadingFailed)
 			}
 
 			// get the document from the dsb instance
@@ -109,10 +113,63 @@ func main() {
 			content, err := document.Download()
 
 			if err != nil {
-				handle(err, ExitDSBLoadingFailed)
+				handle(err, ExitLoadingFailed)
 			}
 
 			data = string(content)
+		} else if argv.Source == "effner" {
+			if argv.Pass == "" {
+				fmt.Println("Error: Effner-Source requires password!")
+				os.Exit(ExitInvalidArgs)
+			}
+
+			if argv.Parser != "effner-de" {
+				fmt.Println("Error: Effner-Source only works with effner-de parser!")
+				os.Exit(ExitInvalidArgs)
+			}
+
+			// TODO Move this somewhere else.
+			form := url.Values{}
+			form.Add("post_password", argv.Pass)
+
+			// load the html from effner.de
+			req, err := http.NewRequest("POST", "https://effner.de/wp-login.php?action=postpass", strings.NewReader(form.Encode()))
+
+			if err != nil {
+				handle(err, ExitLoadingFailed)
+			}
+
+			req.Header.Set("Referer", "https://effner.de/service/vertretungsplan/")
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+			jar, err := cookiejar.New(nil)
+
+			httpClient := &http.Client{
+				Jar: jar,
+			}
+
+			res, err := httpClient.Do(req)
+
+			if err != nil {
+				handle(err, ExitLoadingFailed)
+			}
+
+			link := res.Header.Get("Link")
+			linkParts := strings.Split(link, ",")
+
+			final := strings.Split(linkParts[1], ";")[0]
+			final = final[2 : len(final)-1]
+
+			req, err = http.NewRequest("GET", final, nil)
+			res, err = httpClient.Do(req)
+
+			body, err := io.ReadAll(res.Body)
+
+			if err != nil {
+				handle(err, ExitLoadingFailed)
+			}
+
+			data = string(body)
 		}
 
 		plans, err := parser.Parse(data)
