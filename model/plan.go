6807afb5ed167158bbdd1decac0fc8a12a75150b
Yannick <yan@effner.app>
2023-11-13 01:12:39 +0100

    added bson tags to plan type

--- model/plan.go
+++ model/plan.go
@@ -3,23 +3,23 @@ package model
 import "time"
 
 type Plan struct {
-	Title         string         `json:"title"`
-	Date          string         `json:"date"`
-	CreatedAt     time.Time      `json:"created_at"`
-	Absent        []Absent       `json:"absent"`
-	Substitutions []Substitution `json:"substitutions"`
+	Title         string         `json:"title" bson:"title"`
+	Date          string         `json:"date" bson:"date"`
+	CreatedAt     time.Time      `json:"created_at" bson:"created_at"`
+	Absent        []Absent       `json:"absent" bson:"absent"`
+	Substitutions []Substitution `json:"substitutions" bson:"substitutions"`
 }
 
 type Absent struct {
-	Class   string `json:"class"`
-	Periods string `json:"absent_time"`
+	Class   string `json:"class" bson:"class"`
+	Periods string `json:"absent_time" bson:"periods"`
 }
 
 type Substitution struct {
-	Class      string `json:"class"`
-	Teacher    string `json:"teacher"`
-	Period     string `json:"period"`
-	Substitute string `json:"substitute,omitempty"`
-	Room       string `json:"room,omitempty"`
-	Info       string `json:"info,omitempty"`
+	Class      string `json:"class" bson:"class"`
+	Teacher    string `json:"teacher" bson:"teacher"`
+	Period     string `json:"period" bson:"period"`
+	Substitute string `json:"substitute,omitempty" bson:"substitute,omitempty"`
+	Room       string `json:"room,omitempty" bson:"room,omitempty"`
+	Info       string `json:"info,omitempty" bson:"info,omitempty"`
 }
