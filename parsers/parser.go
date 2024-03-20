8958266caaafba3ec4e52c3b821d59bf776f22ed
Yannick <yan@effner.app>
2024-03-20 14:53:25 +0100

    added GetParserFromType and GetSourceFromType to allow more elegant api usage outside this project

--- parsers/parser.go
+++ parsers/parser.go
@@ -1,7 +1,21 @@
 package parsers
 
-import "github.com/EffnerApp/subparser/model"
+import (
+	"errors"
+	"github.com/EffnerApp/subparser/model"
+)
 
 type Parser interface {
 	Parse(content string) ([]*model.Plan, error)
+}
+
+func GetParserFromType(parserType string) (Parser, error) {
+	switch parserType {
+	case "effner":
+		return &EffnerDSBParser{}, nil
+	case "effner-de":
+		return &EffnerDEParser{}, nil
+	default:
+		return nil, errors.New("unknown parser type '" + parserType + "'")
+	}
 }
--- source/source.go
+++ source/source.go
@@ -1,5 +1,27 @@
 package source
 
+import "errors"
+
 type Source interface {
 	Load() (string, error)
+}
+
+func GetSourceFromType(sourceType string, user string, pass string, path string) (Source, error) {
+	switch sourceType {
+	case "dsb":
+		return &DSBSource{
+			User: user,
+			Pass: pass,
+		}, nil
+	case "file":
+		return &FileSource{
+			Path: path,
+		}, nil
+	case "effner":
+		return &EffnerDESource{
+			Password: pass,
+		}, nil
+	default:
+		return nil, errors.New("unknown source type '" + sourceType + "'")
+	}
 }
